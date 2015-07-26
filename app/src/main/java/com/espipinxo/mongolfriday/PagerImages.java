6cd6ad70b54971a176337386063225838456ba70
Jordi Espinosa <espi@espipinxo.com>
2015-07-27 00:25:52 +0200

    Add clickable on url links in About menu option.

--- app/src/main/java/com/espipinxo/mongolfriday/PagerImages.java
+++ app/src/main/java/com/espipinxo/mongolfriday/PagerImages.java
@@ -93,7 +93,21 @@ public class PagerImages extends ActionBarActivity {
 
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            new ShowAbout(getLayoutInflater().inflate(R.layout.activity_about, null, false)).showAbout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     private void setShareIntent(Intent shareIntent) {
 
         if (mShareActionProvider != null) {
